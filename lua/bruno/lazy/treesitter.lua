2813a974b33b8b7e85ea1cfeeb135602d5f1bc25
BrunoCrestani <bruno.crestani@ufpr.br>
2024-06-26 02:15:55 +0000

    Correction of Lsp and trouble, addition of fileBrowser

--- lua/bruno/lazy/treesitter.lua
+++ lua/bruno/lazy/treesitter.lua
@@ -5,7 +5,7 @@ return {
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
-                "javascript", "java", "c", "lua", "bash",
+                "javascript", "java", "c", "lua", "bash", "python", "html", "css", "json"
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
